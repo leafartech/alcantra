c5d59d478dae0d98ae8bc8065b494d5baa068fbb
leafartech <119262053+leafartech@users.noreply.github.com>
2024-04-12 10:13:07 -0300

    alt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,10 +21,22 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-            <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
-          </head>
-          <body className={`${roboto.className}`}>{children}</body>
-        </html>
-        );
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
+        <script dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PDH9B63D')
+        `}} />
+      </head>
+      <body className={`${roboto.className}`}>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PDH9B63D"
+          height="0" width="0"></iframe></noscript>
+        {children}
+      </body>
+    </html>
+  );
 }
